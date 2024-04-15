9644bee761c99a99b73e4afe288869283ecc4e66
ChenXi <m18268323892@163.com>
2024-04-16 02:24:55 +0800

    [add]完成主页

--- pages/basic/dashboard/dashboard.js
+++ pages/basic/dashboard/dashboard.js
@@ -1,16 +1,25 @@
 //Page Object
 Page({
-  data: {},
-  //options(Object)
-  onLoad: function (options) {},
-  onReady: function () {},
-  onShow: function () {},
-  onHide: function () {},
-  onUnload: function () {},
-  onPullDownRefresh: function () {},
-  onReachBottom: function () {},
-  onShareAppMessage: function () {},
-  onPageScroll: function () {},
-  //item(index,pagePath,text)
-  onTabItemTap: function (item) {},
+  data: {
+    tabValue: '0',
+    tabBarValue: 'home',
+    list: [
+      { value: 'home', label: '首页' },
+      { value: 'search', label: '查询' },
+      { value: 'app', label: '应用' },
+      { value: 'user', label: '我的' },
+    ],
+  },
+  // 选项卡更新
+  onTabChange(e) {
+    this.setData({
+      tabValue: e.detail.value,
+    })
+  },
+  // 导航更新
+  onTabBarChange(e) {
+    this.setData({
+      tabBarValue: e.detail.value,
+    })
+  },
 })
