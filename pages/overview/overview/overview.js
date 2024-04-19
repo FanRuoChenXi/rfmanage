b35cc0299192d5e0ee175a4a3fe1dcfe8c812077
ChenXi <m18268323892@163.com>
2024-04-20 02:30:01 +0800

    [add] 新增许可证总览

--- pages/overview/overview/overview.js
+++ pages/overview/overview/overview.js
@@ -1,16 +1,46 @@
 //Page Object
 Page({
-  data: {},
-  //options(Object)
-  onLoad: function (options) {},
-  onReady: function () {},
-  onShow: function () {},
-  onHide: function () {},
-  onUnload: function () {},
-  onPullDownRefresh: function () {},
-  onReachBottom: function () {},
-  onShareAppMessage: function () {},
-  onPageScroll: function () {},
-  //item(index,pagePath,text)
-  onTabItemTap: function (item) {},
+  data: {
+    tabValue: '0',
+  },
+
+  // 选项卡更新
+  async onTabChange(e) {
+    const newValue = e.detail.value
+    const { tabValue } = this.data
+    if (tabValue == newValue) return false
+    switch (newValue) {
+      case '1':
+        if (this.data['licensesList']) break
+        const licensesList = await getActionList()
+        this.setData({
+          licensesList: licensesList,
+        })
+    }
+    this.setData({
+      tabValue: newValue,
+    })
+  },
 })
+
+// 获取许可证列表
+async function getActionList() {
+  const param = {
+    limit: 5,
+    offset: 0,
+    sort: 'created_at',
+    order: 'asc',
+  }
+  const [res, err] = await wx.$get('licenses', param) // 获取房单客人列表
+  if (err) return wx.$msg(err)
+  const { total, rows } = res
+  const licensesList = []
+  rows.forEach((e) => {
+    licensesList.push({
+      id: e['id'],
+      name: e['name'],
+      manufacturer: e['manufacturer'],
+    })
+  })
+  return licensesList
+}
