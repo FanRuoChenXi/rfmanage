ff05ef880394acfd983f2bbec5b74b43bf1fc8a9
ChenXi <m18268323892@163.com>
2024-04-21 03:46:02 +0800

    [add] 完成登录逻辑

--- pages/basic/login/login.js
+++ pages/basic/login/login.js
@@ -1,6 +1,7 @@
 //Page Object
 Page({
   data: {
+    account: '', // 账号
     password: '', // 密码
     buttonLoading: false, // 按钮加载
     buttonText: '登录',
@@ -10,14 +11,15 @@ Page({
 
   // 登录
   async onSubmitPassword() {
-    // let { password, buttonLoading, buttonText } = this.data
-    // console.log(password)
-    // this.setData({
-    //   buttonLoading: true,
-    //   buttonText: '登录中',
-    // })
-
-    const result = await wx.$get('companies')
-    console.log(result)
+    let { account, password, buttonLoading, buttonText } = this.data
+    if (account == 'admin' && password == 'password') {
+      this.setData({
+        buttonLoading: true,
+        buttonText: '登录中',
+      })
+      wx.$replace('/pages/basic/dashboard')
+    } else {
+      wx.$msg('用户名或密码错误')
+    }
   },
 })
