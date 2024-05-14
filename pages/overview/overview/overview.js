df78c89d356f300ad38b9b96065d4ba3f6a57f4f
ChenXi <m18268323892@163.com>
2024-05-15 04:24:09 +0800

    [fix] 更新总览页删除功能

--- pages/overview/overview/overview.js
+++ pages/overview/overview/overview.js
@@ -70,6 +70,76 @@ Page({
     // console.log(key, id)
     wx.$push('/pages/overview/item-detail', { key, id })
   },
+
+  // 删除按钮
+  onDelete(e) {
+    console.log(e)
+    const { key, id } = e.currentTarget.dataset
+    const modeText = {
+      hardware: '资产',
+      licenses: '许可证',
+      accessories: '配件',
+      consumables: '消耗品',
+      components: '组件',
+    }
+    wx.$modal({
+      title: `删除${modeText[key]}`,
+      showCancel: true,
+      content: '确定要删除吗?',
+    }).then(() => {
+      wx.$loading('提交中...')
+      this.DeleteItem(key, id)
+    })
+  },
+
+  // 删除项目
+  DeleteItem(key, id) {
+    const url = `https://develop.snipeitapp.com/api/v1/${key}/${id}`
+    wx.request({
+      url,
+      method: 'DELETE',
+      header: {
+        Authorization:
+          'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiZTU2MDc0MjVmYjM5YTEwYjFjNTZlZTAxMTBmZDk4ZjQ0ZjVjODMzYjcxZWVhYjZlNDk1NGMwOThlY2YzMzU2MDY4Mzg4MmFhMDMzOTAzNzciLCJpYXQiOjE2MzI4NjU5MTgsIm5iZiI6MTYzMjg2NTkxOCwiZXhwIjoyMjY0MDIxNTE4LCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.LgGVzyH67IRhXvccHd4j2Dn6TDuIuQTBoo30_wD9jPehy8v_h0xBmE1-dOUBRJyeJOI8B4gwPeALsWaudpGj9Lb5qWAtKV7eYtH9IYQKoLF_iHgOGXnAUcNwID6zBU_YyLNSI6gp8zjutLJias33CBLsHy5ZRNpxVibVrZouJ_HjYuIYbtZyLus-KFFeibtZoPiTWOeHhQFD37MR6ifx4dBqT37fN-xDS99mONtrkAplEIou5aSO1oZ4IlJIPCUyA1lixPgpn1YU7PxiBDZp1teeugD0WEmrAqxRS2I0bH4qPsuTsrVXS_lo87Sf5LBGLW7lGHKqyYH6J47OZOM0K-SrxLKtE1ww8jyLBgnnxH0lJHRLCBiwUnL5ZGTUmiOysUA-wSJ6s78o8Pc-ec6bpBvAlelHdiQ-wslE7gzEJDptbejFg-75b_CEwgJYh7J2D18ul6Qu5EFCUEgt033mm04dgVk0isWTDt6EW5ZvTo5Qhr1LY0YnEIXCTqIRN-BSQjL55sZaCrtwR_21bnBGgniyI5MRDYblFawVmFKroeClCpSjBo9vi66akdD5hjpvx67RL3r33BZQhEXmPifUPNH5wP_U-IHGFUD99TJk2c1awF0RASveZRLSunbJb1x6hGAVUaIvQV4r2quWzXqYyKLph9kGTyJYrb6iJtH5smE',
+        Accept: 'application/json',
+      },
+      success: async (response) => {
+        console.log(response)
+        if (response.statusCode == 200) {
+          wx.$msg('删除成功')
+          // 更新列表
+          switch (key) {
+            case 'hardware':
+              this.setData({ assetList: [] })
+              initPagination()
+              this.updateAssetList()
+              break
+            case 'licenses':
+              const licensesList = await getActionList()
+              this.setData({ licensesList })
+              break
+            case 'accessories':
+              const accessoryList = await getAccessoryList()
+              this.setData({ accessoryList })
+              break
+            case 'consumables':
+              const consumableList = await getConsumableList()
+              this.setData({ consumableList })
+              break
+            case 'components':
+              const componentList = await getComponentList()
+              this.setData({ componentList })
+              break
+          }
+        } else {
+          wx.$msg(response.errMsg)
+        }
+      },
+      fail: (error) => {
+        console.log(error)
+      },
+    })
+  },
 })
 
 // 获取资产列表
