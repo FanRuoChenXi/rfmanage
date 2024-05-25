da19e5701949deddc4d769f00a3bb8ebb16909de
ChenXi <m18268323892@163.com>
2024-05-26 04:03:37 +0800

    [fix] 更新数据趋势

--- pages/manage/data-trend/data-trend.js
+++ pages/manage/data-trend/data-trend.js
@@ -10,13 +10,16 @@ Page({
 
   async onLoad() {
     const pieData = await getPieData()
+    const pieData2 = await getPieData2()
     this.initChart(pieData)
+    this.initChart2(pieData2)
   },
 
+  // 饼图1
   initChart(pieData) {
     // 绑定组件
     this.pieComponent = this.selectComponent('#mychart-dom-pie')
-    // 初始化柱状图
+    // 初始化饼图
     this.pieComponent.init((canvas, width, height, dpr) => {
       // 初始化图表
       const chart = echarts.init(canvas, null, {
@@ -28,12 +31,28 @@ Page({
       return chart
     })
   },
+
+  // 饼图2
+  initChart2(pieData) {
+    // 绑定组件
+    this.pieComponent = this.selectComponent('#mychart-dom-pie2')
+    // 初始化饼图
+    this.pieComponent.init((canvas, width, height, dpr) => {
+      // 初始化图表
+      const chart = echarts.init(canvas, null, {
+        width: width,
+        height: height,
+        devicePixelRatio: dpr, // 解决模糊显示问题
+      })
+      chart.setOption(chartsOption.pieOption('资产类别', pieData)) // 根据从后端获取pieData数据,动态更新图表
+      return chart
+    })
+  },
 })
 
-// 获取图表数据
+// 获取图表1数据
 async function getPieData() {
   const data = []
-  const nameText = {}
   const [res, err] = await wx.$get('statuslabels/assets/name')
   if (err) return wx.$msg(err)
   const { labels, datasets } = res
@@ -44,6 +63,28 @@ async function getPieData() {
     data.push({
       value,
       name,
+    })
+  })
+  return data
+}
+
+// 获取图表2数据
+async function getPieData2() {
+  const param = {
+    limit: 10,
+    offset: 0,
+    sort: 'created_at',
+    order: 'desc',
+    categoryType: 'asset',
+  }
+  const data = []
+  const [res, err] = await wx.$get('categories', param)
+  if (err) return wx.$msg(err)
+  const { total, rows } = res
+  rows.forEach((e) => {
+    data.push({
+      value: e['assetsCount'],
+      name: e['name'],
     })
   })
   return data
